147f3fa7f948fd6e35ab8cd74eb6accd936f69ae
busemorose <guillaume.cinkus@gmail.com>
2022-03-30 10:27:28 +0200

    For check() NOTE: add GlobalVariables() at top level for non standard evaluation in dplyr

---
+++ R/global_variables.R
@@ -0,0 +1,4 @@
+globalVariables(c("Sd", "Mean", "Q90", "Q10", "Min", "Max", "CV", "discharge", "Distance", "alpha_mean", "k_max",
+                  "IR", "cv", "svc", "ME", "RT", ".", "runid", "type", "memory_effect", "regulation_time",
+                  "System", "Class", "phi_t", "psi_t", "sim_discharge", "ref", "discharge_ordered_unique",
+                  "y_class", "quant", "value", "variable", "date_hour"))
