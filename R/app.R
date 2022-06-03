f20739a3417680d6ffed151f5372818b6517a34c
busemorose <guillaume.cinkus@gmail.com>
2022-06-03 14:19:29 +0200

    Add hyperlink on banner logo

--- R/app.R
+++ R/app.R
@@ -245,13 +245,13 @@ ui <- fluidPage(
     
     tags$script(
       HTML("var header = $('.navbar > .container-fluid');
-header.append('<div style=\"float:right\"><img src=\"extdata/KARMA_logo.png\" style=\"float:right; height:43px; padding-top:8px; padding-right:5px;\"> </div>');
+header.append('<div style=\"float:right\"><a href=\"http://karma-project.org/\"><img src=\"extdata/KARMA_logo.png\" style=\"float:right; height:43px; padding-top:8px; padding-right:5px;\"></a> </div>');
     console.log(header)")
     ),
     
     tags$script(
       HTML("var header = $('.navbar > .container-fluid');
-header.append('<div style=\"float:right\"><img src=\"extdata/SNOKARST_logo.png\" style=\"float:right; height:43px; padding-top:8px; padding-right:5px;\"> </div>');
+header.append('<div style=\"float:right\"><a href=\"https://sokarst.org/\"><img src=\"extdata/SNOKARST_logo.png\" style=\"float:right; height:43px; padding-top:8px; padding-right:5px;\"></a> </div>');
     console.log(header)")
     )
     
